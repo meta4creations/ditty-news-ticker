4ca772a298011fe64ffd7e7d99f23541d71f68e6
Joe Anderson <joe@metaphorcreations.com>
2021-11-13 22:15:36 +0000

    File moves

--- inc/admin/upgrades.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-/* No upgrades yet! */
--- inc/static/js/script-admin.js
+++
@@ -1,8 +0,0 @@
-/* global CodeMirror:true */
-/* global wp:true */
-/* global ditty_news_ticker_vars:true */
-/* global ajaxurl:true */
-/* global tinyMCE:true */
-
-// @codekit-append 'partials/mtphr-dnt-affix.js';
-// @codekit-append 'partials/admin-general.js';
