4aaa6204581fd23b9c84bb07b70e58fc2144b3d7
Joe Anderson <joe@metaphorcreations.com>
2021-11-19 12:09:40 +0000

    3.0 Updates

--- includes/layout-tags.php
+++ includes/layout-tags.php
@@ -1,5 +1,69 @@
 <?php
+
+/**
+ * Return a rendered link
+ *
+ * @since    3.0
+ * @var      html
+*/
+function ditty_layout_render_tag_link( $link, $html, $class, $data, $atts ) {
+	$link_defaults = array(
+		'url'					=> '',
+		'target'			=> '',
+		'rel'					=> '',
+		'title'				=> '',
+	);
+	$link_args = shortcode_atts( $link_defaults, $link );
 	
+	$defaults = array(
+		'link_before'	=> '',
+		'link_after'	=> '',
+	);
+	$args = shortcode_atts( $defaults, $atts );
+	
+	$link_before = ( '' != $args['link_before'] ) ? "<span class='{$class}__before'>" . sanitize_text_field( $args['link_before'] ) . '</span>' : '';
+	$link_after = ( '' != $args['link_after'] ) ? "<span class='{$class}__after'>" . sanitize_text_field( $args['link_after'] ) . '</span>' : '';
+	
+	$html = sprintf( '<a href="%4$s" class="%5$s" target="%6$s" rel="%7$s" title="%8$s">%2$s%1$s%3$s</a>', $html, $link_before, $link_after, $link_args['url'], $class, $link_args['target'], $link_args['rel'], $link_args['title'] );
+	
+	return $html;
+}
+
+/**
+ * Return a rendered wrapper
+ *
+ * @since    3.0
+ * @var      html
+*/
+function ditty_layout_render_tag_wrapper( $html, $class = '', $atts = array(), $custom_wrapper = false ) {
+	$defaults = array(
+		'wrapper' => false,
+		'before'	=> '',
+		'after'		=> '',
+		'class'		=> '',
+	);
+	$args = shortcode_atts( $defaults, $atts );
+	$before = ( '' != $args['before'] ) ? "<span class='{$class}__before'>" . sanitize_text_field( $args['before'] ) . '</span>' : '';
+	$after = ( '' != $args['after'] ) ? "<span class='{$class}__after'>" . sanitize_text_field( $args['after'] ) . '</span>' : '';
+	
+	if ( isset( $args['wrapper'] ) && false != boolval( $args['wrapper'] ) ) {
+		if ( $custom_classes = $args['class'] ) {
+			$class .= ' ' . $custom_classes;
+		}
+		$html = sprintf( '<%4$s class="%5$s">%2$s%1$s%3$s</%4$s>', $html, $before, $after, $args['wrapper'], esc_attr( $class ) );
+	}
+	if ( $custom_wrapper ) {
+		$html = sprintf( $custom_wrapper, $html );
+	}
+	return $html;
+}
+
+/**
+ * The custom item content
+ *
+ * @since    3.0
+ * @var      html
+*/	
 function ditty_layout_default_tag_content( $data, $atts, $custom_wrapper = false ) {
 	if ( ! isset( $data['content'] ) ) {
 		return false;
@@ -30,6 +94,12 @@ function ditty_layout_default_tag_content( $data, $atts, $custom_wrapper = false
 	return $html;
 }
 
+/**
+ * The custom item content
+ *
+ * @since    3.0
+ * @var      html
+*/	
 function ditty_layout_wp_editor_tag_content( $data, $atts, $custom_wrapper = false ) {
 	if ( ! isset( $data['content'] ) ) {
 		return false;
