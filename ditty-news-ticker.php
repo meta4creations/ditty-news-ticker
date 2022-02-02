d93698eb2a36e140929b7fad08f4df2617276a65
Joe Anderson <joe@metaphorcreations.com>
2022-02-02 12:53:48 +0000

    Version update

--- ditty-news-ticker.php
+++ ditty-news-ticker.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Ditty (formerly Ditty News Ticker)
  * Plugin URI:        https://www.metaphorcreations.com/ditty
  * Description:       Formerly Ditty News Ticker. Ditty is a multi-functional data display plugin.
- * Version:           3.0.12
+ * Version:           3.0.13
  * Author:            Metaphor Creations
  * Author URI:        https://www.metaphorcreations.com
  * License:           GPL-2.0+
@@ -20,7 +20,7 @@ if ( ! defined( 'WPINC' ) ) {
 
 // Plugin version.
 if ( ! defined( 'DITTY_VERSION' ) ) {
-	define( 'DITTY_VERSION', '3.0.12' );
+	define( 'DITTY_VERSION', '3.0.13' );
 }
 
 // Plugin Folder Path.
