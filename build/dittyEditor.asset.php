c13fb9e59e03be67bcc33e9bd52a0313a1dd9288
Joe Anderson <joe@metaphorcreations.com>
2022-12-09 09:10:47 -0600

    3.1 Updates

--- build/dittyEditor.asset.php
+++ build/dittyEditor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1a99087b85fc4bbc8805');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3093da1cbb6ff5d55c3f');
--- build/dittyScripts.asset.php
+++ build/dittyScripts.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e7c0ad873fe12aba555f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8e93031f4d014d2d1aef');
