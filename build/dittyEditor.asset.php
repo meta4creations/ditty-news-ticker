64df9a45fa9389b18e99da7b16f579d544f71313
Joe Anderson <joe@metaphorcreations.com>
2023-01-30 16:30:44 -0600

    Layout tag updates

--- build/dittyEditor.asset.php
+++ build/dittyEditor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2d5b7ba84c10eaed39ba');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b91d0a65a4b805b3f21e');
