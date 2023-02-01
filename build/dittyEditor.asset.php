8e4d46aa5003192b7af19b6b632a2362e385482a
Joe Anderson <joe@metaphorcreations.com>
2023-02-01 14:19:18 +0000

    Toastify updates

--- build/dittyEditor.asset.php
+++ build/dittyEditor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'feba9e4d6c157ae0575e');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '39ccf930cc2b49563b8f');
