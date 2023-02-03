7cfb75f975d62a579643a2558595ecae643b6dd3
Joe Anderson <joe@metaphorcreations.com>
2023-02-03 17:41:34 -0600

    Updates

--- build/ditty.asset.php
+++ build/ditty.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-hooks', 'wp-i18n'), 'version' => 'b72dcba5564c2e29d82e');
+<?php return array('dependencies' => array('wp-hooks', 'wp-i18n'), 'version' => '475708d2d19cdaee92f3');
--- build/dittyEditor.asset.php
+++ build/dittyEditor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0a97cd4a78b267a9c562');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '29a7d39cade1e50f94ed');
