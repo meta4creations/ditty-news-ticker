d2f7cc9ee60633461649e49adbc82d1bf1346af9
Joe Anderson <joe@metaphorcreations.com>
2023-04-12 13:30:14 -0500

    Render updates

--- build/dittyEditor.asset.php
+++ build/dittyEditor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'b56ebc5fa21c7a4a8686');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '1910f081126caaded6e2');
--- src/dittyEditor.js
+++ src/dittyEditor.js
@@ -1,13 +1,14 @@
-import { createRoot } from "@wordpress/element";
+const { render } = wp.element; //we are using wp.element here!
 import { EditorProvider } from "./editor/context";
 import App from "./editor/app";
 import "./assets/css/editor.scss";
 
 if (document.getElementById("ditty-editor__wrapper")) {
-  const root = createRoot(document.getElementById("ditty-editor__wrapper"));
-  root.render(
+  //check if element exists before rendering
+  render(
     <EditorProvider>
       <App />
-    </EditorProvider>
+    </EditorProvider>,
+    document.getElementById("ditty-editor__wrapper")
   );
 }
