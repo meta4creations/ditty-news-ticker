0b1eb47cecc2700b6b89c6916a7d4f41ca2cd547
meta4creations <joe@metaphorcreations.com>
2024-02-03 07:54:34 -0600

    Updated tested up to version

--- ditty-news-ticker.php
+++ ditty-news-ticker.php
@@ -11,7 +11,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Requires at least: 5.6
  * Requires PHP:      7.1
- * Tested up to:      6.4.2
+ * Tested up to:      6.4.3
  * Text Domain:       ditty-news-ticker
  * Domain Path:       /languages
  */
